0688a100a03f25510b5add1d56fdb2969dc4a824
barbo186 <107640764+barbo186@users.noreply.github.com>
2022-06-16 19:53:43 +0200

    Add files via upload

---
+++ Configuration.java
@@ -0,0 +1,26 @@
+package PackageName;
+
+import Barbo.Comandi;
+import Barbo.Messaggi;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import javax.security.auth.login.LoginException;
+
+public class ClassName {
+
+    public static void main(String[] args) throws LoginException {
+
+        JDA bot = JDABuilder.createDefault("token-here") // The token must be taken from the "Discord Developer Portal" and must remain secret.
+                .setStatus(OnlineStatus.ONLINE) //SET STATUS BOT
+                //You can use other status: OFFLINE, DO_NOT_DISTURB and UNKNOWN
+                .setActivity(Activity.playing("example")) //SET BOT "ACTION"
+                //You can use: playing, listening, streaming, watching, ecc...
+                .build();
+                System.out.println("API READY!"); //Send message in the terminal
+                //bot.addEventListener(new SMS());       <--- Add this later!
+                //bot.addEventListener(new PING());      <--- Add this later!
+    }
+
+}
